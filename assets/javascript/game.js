31550237d94bb5c62229570300b8671668442334
Khorton007 <Khorton007@gmail.com>
2018-12-06 20:20:19 -0700

    Psychic Game update

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+﻿
+function randomString() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var string_length = 8;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	document.randform.randomfield.value = randomstring;
+}
+</script>
+
+<form name="randform">
+<input type="button" value="Create Random String" onClick="randomString();">&nbsp;
+<input type="text" name="randomfield" value="">
+</form>
+
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [254, 45, 212, 365, 2543];
+console.log(random_item(items));
